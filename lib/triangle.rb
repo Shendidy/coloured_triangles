ba1bf92b6808b5c30024777146f98d233e611c92
Shendidy <sherif@shendidy.com>
2019-05-06 04:29:43 +0100

    completed application requirements

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,26 @@
 class Triangle
   def triangle(str)
-    "R"
+    str = str.split("")
+    return str.join("") unless str.length > 1
+
+    arr = []
+    i = 0
+    while i < (str.length - 1)
+      arr << get_col([str[i], str[i+1]])
+      i += 1
+    end
+    triangle(arr.join(""))
+  end
+
+  private
+
+  def get_col(arr2)
+    return arr2[0] unless arr2[0] != arr2[1]
+    if arr2.include? "G"
+      return "R" if arr2.include? "B"
+      return "B" if arr2.include? "R"
+    else
+      return "G"
+    end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,7 +1,27 @@
 require 'triangle'
 
 describe Triangle do
-  it '1= should return R' do
+  it '1- should return R' do
     expect(subject.triangle('GB')).to eq('R')
+  end
+
+  it '2- should return R' do
+    expect(subject.triangle('RRR')).to eq('R')
+  end
+
+  it '3- should return B' do
+    expect(subject.triangle('RGBG')).to eq('B')
+  end
+
+  it '4- should return G' do
+    expect(subject.triangle('RBRGBRB')).to eq('G')
+  end
+
+  it '5- should return G' do
+    expect(subject.triangle('RBRGBRBGGRRRBGBBBGG')).to eq('G')
+  end
+
+  it '6- should return B' do
+    expect(subject.triangle('B')).to eq('B')
   end
 end
